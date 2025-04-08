2f6a6b6502b8d53432bc1f0569bfdab0bc150b18
王小龙 <2681977867>
2025-04-08 15:57:09 +0800

    删掉原来的获取文本

--- app.py
+++ app.py
@@ -649,7 +649,7 @@ if __name__ == '__main__':
     appasync.router.add_post("/record", record)
     appasync.router.add_post("/is_speaking", is_speaking)
     appasync.router.add_static('/', path='web')
-    appasync.router.add_get("/get_llm_output", get_llm_output)
+
 
     # Configure default CORS settings.
     cors = aiohttp_cors.setup(appasync, defaults={
