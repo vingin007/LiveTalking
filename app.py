d3d52acb0e44fef7500fa811a28a6c9db2599036
王小龙 <2681977867>
2025-11-18 18:13:46 +0800

    修复预检失败

--- app.py
+++ app.py
@@ -668,15 +668,6 @@ if __name__ == '__main__':
     appasync.router.add_get("/sse", sse_handler)
     appasync.router.add_get("/get_llm_output", get_llm_output)
 
-
-    async def preflight_handler(request):
-        return web.Response()
-
-
-    appasync.router.add_options("/human", preflight_handler)
-    appasync.router.add_options("/stop", preflight_handler)
-    appasync.router.add_options("/offer", preflight_handler)
-
     # Configure default CORS settings.
     cors = aiohttp_cors.setup(appasync, defaults={
         "*": aiohttp_cors.ResourceOptions(
